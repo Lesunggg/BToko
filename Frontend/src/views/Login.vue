84a36dcad6e11c379118b0283c0852eec9d009d4
Lesunggg <108666703+Lesunggg@users.noreply.github.com>
2024-01-16 23:32:41 +0700

    Delete Frontend directory

--- Frontend/src/views/Login.vue
+++
@@ -1,6 +0,0 @@
-<script setup>
-  import Form from '../components/form.vue'
-</script>
-<template>
-  <Form form="Login"></Form>
-</template>
--- Frontend/src/views/Regis.vue
+++
@@ -1,6 +0,0 @@
-<script setup>
-  import Form from '../components/form.vue'
-</script>
-<template>
-  <Form form="Register"></Form>
-</template>
