6549a488fbf1699900404f7edcf5b44f0e04d87b
Lesunggg <108666703+Lesunggg@users.noreply.github.com>
2024-01-17 23:19:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const routerUser = require('./router/user')
 app.use(express.text())
 app.use(express.json())
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://toko-trio.netlify.app",
   methods: ["POST", "PUT", "GET", "DELETE", "OPTIONS", "HEAD"],
   credentials: true,
 }))
