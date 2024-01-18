3b4d486dacd5618e04eefa2ed300987385091cb0
Lesunggg <108666703+Lesunggg@users.noreply.github.com>
2024-01-18 16:05:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(session({
   saveUninitialized:true,
   cookie:{
     maxAge: 1000*60*60*12,
-    domain: 'toko-trio.netlify.app',
+    domain: "https://toko-trio.netlify.app",
     secure: true,
     sameSite:"none",
     httpOnly:true}
