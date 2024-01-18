ee000cc5899b1f427b4c528c0a1ff597565b8123
Lesunggg <108666703+Lesunggg@users.noreply.github.com>
2024-01-19 00:38:25 +0700

    Update middleware.js

--- router/middleware.js
+++ router/middleware.js
@@ -1,30 +1,47 @@
 const express = require('express')
 const router = express.Router()
 const connection = require('./data')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 
 router.use((req,res,next)=>{   
-  if (req.session.user) {
-    const q = `SELECT * FROM user WHERE id=${req.session.user.id}`;
+  token = req.headers.authorization
+  if (token == "") {
+    res.status(404).send('Account not found, Please login first')
+    return
+  }
+  jwt.verify(token, "winnnnn", (err,decoded)=>{
+    if (err) {
+      res.status(401).send('Not Found')
+      return;
+    }
+    const payload = decoded
+    const nama = payload.name
+    const pass = payload.pass
+    const q = `SELECT * FROM user WHERE id=${payload.id}`
     connection.query(q, (err, result)=>{
       if (err) {
         res.status(500).send('Server Problem')
-        return;
+        return
       }
       if (result.length === 0) {
         res.status(404).send('Account not found, Please login first')
+        return
       }
-      const cekUser = result[0].nama
-      const cekPass = result[0].password
-      if (cekUser == req.session.user.nama && cekPass == req.session.user.password) {
-        next()
-      }else{
-        res.send('Profile Not Found')
-      }
+      bcrypt.compare(pass, result[0].password, (err, hasil) => {
+        if (err) {
+          res.status(500).send('Server Problem')
+          return
+        }
+        if (hasil && nama == result[0].nama) {
+          res.locals.data = payload
+          next()
+        }else{
+          res.send('Profile Not Found')
+        }
+      })
     })
-  } 
-  else {
-    res.send('Account not found, Please login first');
-  }
+  })
 })
 
 module.exports = router
